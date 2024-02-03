83990822fb37c7a2cffb1b56a7ba19fb9f18579a
Mateus <mateusff2323@gmail.com>
2024-02-03 16:24:01 -0300

    update

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from tkinter import *
 from tkinter import filedialog
 from pytube import YouTube
+from tkinter import messagebox
 
 # Diretório de saída
 diretorioDeSaida = ""
+mensagemDeErro = '''
+Algo de errado ocorreu...
+Verifique se você está conectado a internet ou se você esqueceu de preencher a URL e escolher o diretório!
+'''
 
 # Configurações iniciais da janela
 janelaPrincipal = Tk()
-janelaPrincipal.title("Made by João Mateus")
+janelaPrincipal.title("Baixar Músicas")
 janelaPrincipal.geometry("700x150")
 janelaPrincipal.resizable(False, False)
 
@@ -19,10 +24,13 @@ def escolherDiretorio():
 
 def baixarMusica():
     global diretorioDeSaida
-    url = localParaColocarUrl.get()
-    url = YouTube(url)
-    musicaParaBaixar = url.streams.get_audio_only()
-    musicaParaBaixar.download(output_path=diretorioDeSaida)
+    try:
+            url = localParaColocarUrl.get()
+            url = YouTube(url)
+            musicaParaBaixar = url.streams.get_audio_only()
+            musicaParaBaixar.download(output_path=diretorioDeSaida)
+    except:
+        messagebox.showerror(title="ERROR", message=mensagemDeErro)
 
 # Elementos
 botaoEscolherDiretorio = Button(janelaPrincipal, text="Escolha o diretório", command=escolherDiretorio)
@@ -35,6 +43,9 @@ textoDoPrimeiroLabel.grid(row=1, column=0, padx=10, pady=10)
 
 botaoBaixarMusica = Button(janelaPrincipal, text="Baixar Música", command=baixarMusica)
 botaoBaixarMusica.grid(row=2, column=0, padx=0, pady=15)
+
+textoComMeuNome = Label(janelaPrincipal, text="Made by João Mateus")
+textoComMeuNome.place(relx=0.41,rely=0.3, relheight=1.2, relwidth=0.99)
 
 localParaColocarUrl = Entry(janelaPrincipal, width=90)
 localParaColocarUrl.grid(row=1, column=1, padx=10, pady=10)
